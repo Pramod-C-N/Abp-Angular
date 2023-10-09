e8830bcb05722df2a6d2740c4bc04841f4fb9058
Pramod-C-N <142775603+Pramod-C-N@users.noreply.github.com>
2023-10-09 11:19:48 +0530

    author dto and crud method done

--- aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -3,10 +3,19 @@
 public static class BookStorePermissions
 {
     public const string GroupName = "BookStore";
+   
 
     public static class Books
     {
         public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
         public const string Create = Default + ".Create";
         public const string Edit = Default + ".Edit";
         public const string Delete = Default + ".Delete";
